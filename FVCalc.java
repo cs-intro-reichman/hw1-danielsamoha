58d5aee6dfa63d66cfb3774b783e26954491d067
Daniel Samoha <danielaharon.samoha@post.runi.ac.il>
2025-11-02 13:31:11 +0200

    update

--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,12 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
 	public static void main(String[] args){
-		// Replace this comment with your code
+
+		int currentValue = Integer.parseInt(args[0]);
+		double rate = Double.parseDouble(args[1]);
+		int n = Integer.parseInt(args[2]);
+
+		System.out.println("After " + n + " years, " + currentValue +
+		 " saved at 10.0% will yield $121");
 	}
 }
