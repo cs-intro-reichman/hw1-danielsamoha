252ba33d49646ccbf4c0c389ccc470157f9209d6
Daniel Samoha <danielaharon.samoha@post.runi.ac.il>
2025-11-02 18:11:20 +0200

    completed

--- FVCalc.java
+++ FVCalc.java
@@ -5,14 +5,15 @@ public class FVCalc {
 		double currentValue = Double.parseDouble(args[0]);
 		double rate = Double.parseDouble(args[1]);
 		int n = Integer.parseInt(args[2]);
-		double newrate = rate + 100.0;
+		double newrate = 1.0 + (rate / 100.0);
 		double futureValue = currentValue;
 
 		for (int i=0; i < n; i++){
-			futureValue = futureValue * (newrate/100);
+			futureValue = futureValue * newrate;
 		}
+		
+		System.out.println("After " + n + " years, $" + (int)currentValue +
+         " saved at " + rate + "% will yield $" + (int)futureValue);
 
-		System.out.println("After " + n + " years, " + currentValue +
-		 " saved at " + rate + "% will yield " + (int)futureValue +"$.");
 	}
 }
